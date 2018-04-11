fc4aeeb84fe034b9890ff370a024c6466a9b47c7
BlisS <contacto@fixter.org>
2018-04-11 01:00:04 -0500

    almost

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,28 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  this.cards = this.shuffleCard(cards);
+  this.pairSelected = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+	var counter = cardsArr.length;
+	while(counter > 0){
+		var index = Math.floor(Math.random() * counter);
+		counter--;
+		temp = cardsArr[counter];
+		cardsArr[counter] = cardsArr[index];
+		cardsArr[index] = temp;
+	}
+	return cardsArr;
+};
 
-// };
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+	this.pairsClicked++;
+	if(firstCard === secondCard) this.pairsGuessed++;
+	return firstCard === secondCard;
+}
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function () {
+	return this.pairsGuessed === 12;
+};
